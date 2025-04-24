af8293261f18a2493c10d780b9e13893e2427f87
ModyAssociates <154823452+ModyAssociates@users.noreply.github.com>
2025-04-24 03:06:15 -0400

    Update printify-products.js

--- project/netlify/functions/printify-products.js
+++ project/netlify/functions/printify-products.js
@@ -1,7 +1,5 @@
 // netlify/functions/printify-products.js
 
-const fetch = require('node-fetch');
-
 exports.handler = async (event) => {
   const token  = process.env.PRINTIFY_API_TOKEN;
   const shopId = process.env.PRINTIFY_SHOP_ID;
